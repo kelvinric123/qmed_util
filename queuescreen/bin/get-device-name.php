b06a6a1c3898be548b7e9952e325341a6865f785
Rahimie Ahmad <newrehmi@gmail.com>
2019-11-26 16:52:22 +0800

    jobby require

--- queuescreen/bin/get-device-name.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$config = json_decode(file_get_contents(__DIR__ . '/../config.json'), true);
-
-echo $config['device_name'];
--- queuescreen/bin/get-host.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$config = json_decode(file_get_contents(__DIR__ . '/../config.json'), true);
-
-echo isset($config['host']) ? $config['host'] : 'http://qmed.asia';
