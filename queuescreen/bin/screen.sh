1abd6e0bab9e08e328d4141e79e89c675ed4211a
Rahimie Ahmad <newrehmi@gmail.com>
2019-11-27 17:54:52 +0800

    autostart/setup is now relative, and command reworked

--- queuescreen/bin/screen.sh
+++
@@ -1,3 +0,0 @@
-INSTALLATION_ID="$(php /home/pi/qmed-utils/queuescreen/bin/settings.php installation_id)"
-HOST="$(php /home/pi/qmed-utils/queuescreen/bin/settings.php host)"
-/usr/bin/chromium-browser --noerrordialogs --incognito --disable-session-crashed-bubble --disable-infobars --kiosk $HOST/queuescreen/$INSTALLATION_ID
--- queuescreen/sources/src/Commands/SetupScreenCommand.php
+++ queuescreen/sources/src/Commands/SetupScreenCommand.php
@@ -2,6 +2,7 @@
 
 namespace Rasque\Commands;
 
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -21,28 +22,32 @@ class SetupScreenCommand extends BaseCommand
         /** @var QuestionHelper $helper */
         $helper = $this->getHelper('question');
 
-        $question = new Question('Which clinic are you setting this Raspberry up for? : ', false);
+        $question = new Question('Which clinic are you setting this Raspberry up for? search keyword : ', false);
 
         if (($search = $helper->ask($input, $output, $question)) === false)
             return $this->execute($input, $output);
 
         $baseUrl = isset($this->config['host']) ? $this->config['host'] : 'https://qmed.asia';
 
-        $result = @file_get_contents($baseUrl . '/api/installation/search?name=' . urlencode($search));
+        $http = new Client([
+            'base_uri' => $baseUrl
+        ]);
 
-        if (!$result)
-            return $this->write($output, 'Ooops, can\'t retrieve api at this moment');
+//        $result = @file_get_contents($baseUrl . '/apis/installations/search?name=' . urlencode($search));
+        $clinics = json_decode($http->request('GET', '/apis/installations/search?name=' . urlencode($search))->getBody(), true)['data'];
+//        if (!$result)
+//            return $this->write($output, 'Ooops, can\'t retrieve api at this moment');
 
         // search the clinic through APIs
-        $result = json_decode($result, true);
+//        $result = json_decode($result, true);
 
-        if (count($result) == 0)
+        if (count($clinics) == 0)
             return $this->write($output, 'Oops, couldn\'t find any clinics');
 
-        if (count($result) > 1) {
+        if (count($clinics) > 1) {
             $choice = [];
 
-            foreach ($result as $index => $item) {
+            foreach ($clinics as $index => $item) {
                 $choice[$index + 1] = $item['name'] . $item['installation_id'];
             }
 
@@ -50,18 +55,42 @@ class SetupScreenCommand extends BaseCommand
 
             $answer = $helper->ask($input, $output, $question);
 
-            $record = $result[array_flip($choice)[$answer] - 1];
+            $record = $clinics[array_flip($choice)[$answer] - 1];
         } else {
-            $record = $result[0];
+            $record = $clinics[0];
         }
 
         if (!$helper->ask($input, $output, new ConfirmationQuestion('Set-up this raspberry for [' . $record['name'] . '](y/n)? : ')))
             return $this->write($output, 'Set-up cancelled');
 
-        if (!trim(($deviceName = $helper->ask($input, $output, new Question('What should we label this queuescreen with? : ')))))
-            return $this->write($output, 'Please write at least something..');
+        // search for existing screens
+        $screens = json_decode($http->request('GET', '/apis/installations/' . $record['installation_id'] . '/screens')->getBody(), true)['data'];
 
-        $this->config['device_name'] = $deviceName;
+        if (count($screens) > 0) {
+            $choices = [];
+
+            $choices[1] = 'Create new screen';
+
+            foreach ($screens as $index => $screen) {
+                $choices[$index + 2] = $screen['name'];
+            }
+
+            $question = new ChoiceQuestion('Oops, we already found existing screens', $choices);
+
+            $answer = $helper->ask($input, $output, $question);
+
+            if ($answer == 'Create new screen') {
+                if (!($screen = $this->createNewScreen($helper, $input, $output, $http, $record['installation_id'])))
+                    return 0;
+            } else {
+                $screen = $screens[array_flip($choices)[$answer] - 2];
+            }
+        } else {
+            if (!($screen = $this->createNewScreen($helper, $input, $output, $http, $record['installation_id'])))
+                return 0;
+        }
+
+        $this->config['screen_id'] = $screen['id'];
         $this->config['installation_id'] = $record['installation_id'];
 
         file_put_contents($this->configPath, json_encode($this->config, JSON_PRETTY_PRINT));
@@ -69,15 +98,46 @@ class SetupScreenCommand extends BaseCommand
         $output->writeLn('Successfully installed!');
         $output->writeLn('');
         $output->writeln('Clinic : ' . $record['name']);
-        $output->writeln('Device Name : ' . $deviceName);
+        $output->writeln('Screen Name : ' . $screen['name']);
         $output->writeln('Installation ID : ' . $record['installation_id']);
-        
-        // import autostart
-        if (!file_exists('/home/pi/.config/autostart/chrome-queuescreen.desktop'))
-            copy($this->basePath . '/autostart/chrome-queuescreen.desktop', '/home/pi/.config/autostart/chrome-queuescreen.desktop');
+
+        // import autostart, and create screen.sh
+        $this->setupAutostart();
+
+
 
         return 1;
     }
 
+    protected function setupAutostart()
+    {
+        // autostart
+        $binPath = realpath($this->basePath . '/bin');
 
+        var_dump($binPath);
+
+        $content = str_replace('BIN_PATH', $binPath, file_get_contents($this->basePath . '/autostart/chrome-queuescreen.desktop'));
+
+        file_put_contents('/home/pi/.config/autostart/chrome-queuescreen.desktop', $content);
+
+        // screen.sh
+        $stub = file_get_contents($binPath . '/screen.sh.stub');
+
+        file_put_contents($binPath . '/screen.sh', str_replace('BIN_PATH', $binPath, $stub));
+    }
+
+    protected function createNewScreen(QuestionHelper $helper, $input, $output, Client $http, $installationId)
+    {
+        $screenName = $helper->ask($input, $output, new Question('What should we label this queuescreen with? : '));
+
+        if (!$screenName)
+            return $this->createNewScreen($helper, $input, $output, $http, $installationId);
+
+        $screen = json_decode($http->request('POST', '/apis/installations/' . $installationId . '/screens', [
+            'json' => ['name' => $screenName]
+        ])->getBody(), true)['data'];
+
+        return $screen;
+
+    }
 }
