da8a04c864bbabc598c7025752437e46509e31ab
Remi <newrehmi@gmail.com>
2019-12-05 12:30:17 +0800

    added wifi signals info

--- queuescreen/sources/src/ResourceParam.php
+++ queuescreen/sources/src/ResourceParam.php
@@ -31,11 +31,63 @@ class ResourceParam
 		}
 	}
 	
+	// https://github.com/pear/Net_Wifi/blob/master/Net/Wifi.php
+	public function getConnectionStatus()
+	{
+		$iwconfig = shell_exec('/sbin/iwconfig');
+	
+		$fields = [
+			'bit_rate' => '/Bit Rate[:=]([0-9.]+) [mk]b\\/s/i',
+			'link_quality' => '/Link Quality[:=](-?[0-9]+)/'
+		];
+		
+		$status = [];
+		
+		foreach ($fields as $key => $regex) {
+			if (preg_match($regex, $iwconfig, $arMatches)) {
+				$status[$key] = $arMatches[1];
+			}
+		}
+		
+		if (isset($status['link_quality']))
+			$status['link_quality'] = round(((int) $status['link_quality']) / 70, 2);
+		
+        return $status;
+	}
+	
+	public function getStorage($partition = '/dev/root')
+	{
+		$memLines = explode("\n", shell_exec('df'));
+		
+		$mem = [];
+
+		foreach ($memLines as $line) {
+			if (strpos($line, $partition) === false)
+				continue;
+				
+			$parts = preg_split('/\s+/', $line);
+			
+			return [
+				'total' => $parts[1],
+				'used' => $parts[2],
+				'available' => $parts[3]
+			];
+		}
+	}
+
+	public function hasInternet()
+	{
+		return @file_get_contents('https://google.com') ? true : false;
+	}
+	
 	public function toArray()
 	{
 		return [
 			'temp' => $this->getTemp(),
 			'memory' => $this->getMemory(),
+			'storage' => $this->getStorage(),
+			'connection_status' => $this->getConnectionStatus(),
+			'has_internet' => $this->hasInternet()
 		];
 	}
 }
