5608245f23e5058047f5761f46ebe472315091ed
Remi <newrehmi@gmail.com>
2019-12-10 17:51:56 +0800

    autorestart on inactivity

--- queuescreen/bin/cron.php
+++ queuescreen/bin/cron.php
@@ -2,17 +2,33 @@
 
 require_once __DIR__ . '/../sources/vendor/autoload.php';
 
+$app = \Rasque\App::instance();
+
 $jobby = new \Jobby\Jobby();
 
 $jobby->add('Ping', [
     'closure' => function() {
-        $config = json_decode(file_get_contents(__DIR__ . '/../config.json'), true);
-
-        $base = isset($config['host']) ? $config['host'] : 'https://qmed.asia';
-
-        @file_get_contents($base . '/api/installation/' . $config['installation_id'] . '/ping/device/' . $config['device_name']);
+        \Rasque\Logger::create()->log('ping');
     },
    'schedule' => '* * * * *'
+]);
+
+$jobby->add('RunningCheck', [
+    'closure' => function() use ($app) {
+        $time = @file_get_contents($app->getBasePath() . '/running-timestamp');
+        
+        // skip first
+        if (!$time)
+            return;
+            
+        // if in-active for the last 10 minutes, restart
+        if (time() > strtotime('+5 minutes', $time)) {
+            \Rasque\Logger::create()->log('inactive_reboot');
+            shell_exec('sudo reboot -f');
+            return;         
+        }
+    },
+    'schedule' => '*/5 * * * *'
 ]);
 
 $jobby->run();
--- queuescreen/www/dev/resource.php
+++ queuescreen/www/dev/resource.php
@@ -8,4 +8,9 @@ require_once __DIR__ . '/../../sources/vendor/autoload.php';
 
 header('Content-Type: application/json');
 
+$app = \Rasque\App::instance();
+
+// update timestamp each time this file is accessed
+file_put_contents($app->getBasePath() . '/running-timestamp', time());
+
 echo json_encode((new \Rasque\ResourceParam())->toArray(), JSON_PRETTY_PRINT);
