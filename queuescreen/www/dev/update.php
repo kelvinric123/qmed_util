b573a9369b6585ee8395b56aafb2ca3344ff0ecc
Rahimie Ahmad <newrehmi@gmail.com>
2019-12-15 20:13:37 +0800

    device update

--- queuescreen/www/dev/update.php
+++ queuescreen/www/dev/update.php
@@ -9,7 +9,7 @@ $app = App::instance();
 // run code update
 $path = $app->getPath('bin/update.sh');
 
-$app->log('device_update' . isset($_GET['version']) ? '_' . $_GET['version'] : '');
+$app->log('device_update' . (isset($_GET['version']) ? '_' . $_GET['version'] : ''));
 
 if (isset($_GET['version']))
     shell_exec('sh ' . $path . ' ' . $_GET['version']);
