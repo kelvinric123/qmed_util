94ce54b12c087fc689c6cd64b431ab076ec23e20
Rahimie Ahmad <newrehmi@gmail.com>
2019-12-19 16:02:39 +0800

    device_update log moved up

--- queuescreen/www/dev/update.php
+++ queuescreen/www/dev/update.php
@@ -6,6 +6,8 @@ require_once __DIR__ . '/../../sources/vendor/autoload.php';
 
 $app = App::instance();
 
+$app->log('device_update' . (isset($_GET['version']) ? '_' . $_GET['version'] : ''));
+
 if (isset($_GET['soft-update'])) {
     $path = $app->getPath('bin/soft-update.sh');
     shell_exec('sh ' . $path);
@@ -14,8 +16,6 @@ if (isset($_GET['soft-update'])) {
 
 // run code update
 $path = $app->getPath('bin/update.sh');
-
-$app->log('device_update' . (isset($_GET['version']) ? '_' . $_GET['version'] : ''));
 
 if (isset($_GET['version']))
     shell_exec('sh ' . $path . ' ' . $_GET['version']);
